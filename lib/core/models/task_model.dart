ae8b26a0309d1251054ef5ce1d164ccb03014c38
mahmoudradwan11 <102020555+mahmoudradwan11@users.noreply.github.com>
2025-01-22 21:54:01 +0200

    Task model

---
+++ lib/core/models/task_model.dart
@@ -0,0 +1,20 @@
+class TaskModel{
+  String? title;
+  String? subTitle;
+  String? date;
+  String? time;
+  int? priority;
+  String? category;
+  int? color;
+  String? type;
+  TaskModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.time,
+    required this.priority,
+    required this.category,
+    required this.color,
+    required this.type,
+  });
+}
